b07efe99ef890ef757dc1e99929ce390e8238b02
Alex Lee <jihyelee318@gmail.com>
2023-06-20 16:56:55 +0900

    first commit

---
+++ src/ch01/HelloWorld.java
@@ -0,0 +1,14 @@
+package ch01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");      // 'sysout' + ctrl + space
+		System.out.println("안녕하세요?");
+		
+		
+
+	}
+
+}
